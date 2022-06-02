854eb73e9c1f32a0e4c25ef826e49d678aaa34c1
Vikms95 <vms1995@hotmail.com>
2022-06-02 22:40:27 +0200

    Adjust video positioning

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -5,14 +5,16 @@ import heroVideo from './hero-video.mp4';
 function HomePage() {
   return (
     <div className="homepage-container">
-      <video autoPlay loop muted playsInline id="hero-video">
-        <source src={heroVideo} type="video/mp4" />
+      <div className="video-text">
         <div className="video-text-1">
           Your favourite games ...
         </div>
         <div className="video-text-2">
-          ... with the best price you can find.
+          ... at the best price you can find.
         </div>
+      </div>
+      <video autoPlay loop muted playsInline id="hero-video">
+        <source src={heroVideo} type="video/mp4" />
       </video>
     </div>
   );
