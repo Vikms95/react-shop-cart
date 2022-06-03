809f7d13a0e284a41992f043b718bec381e5e51b
Vikms95 <vms1995@hotmail.com>
2022-06-03 14:20:23 +0200

    Implement dynamic dates on url fetch

--- src/utils/urlHandler.tsx
+++ src/utils/urlHandler.tsx
@@ -1,35 +1,29 @@
 const popular = (currentYear) => (
-  `https://api.rawg.io/api/games?
-    page_size=50&dates=${currentYear}-01-01,${currentYear}-12-31&
-    ordering=-added&key=888f6e198d894fcdac3d561150fc3732`
+  `https://api.rawg.io/api/games?page_size=24&dates=${currentYear}-01-01,${currentYear}-12-31&ordering=-added&key=888f6e198d894fcdac3d561150fc3732`
 );
 const highestRated = (currentYear) => (
-  `https://api.rawg.io/api/games?
-    page_size=50dates=${currentYear}-01-01,${currentYear}-12-31&ordering=-rating
-    &key=888f6e198d894fcdac3d561150fc3732`
+  `https://api.rawg.io/api/games?page_size=24&dates=${currentYear}-01-01,${currentYear}-12-31&ordering=-rating&key=888f6e198d894fcdac3d561150fc3732`
 );
 
-const lastReleased = (currentYear) => (
-  `https://api.rawg.io/api/games?
-    page_size=50&dates=2018-01-01,2018-12-31&ordering=-added
-    &key=888f6e198d894fcdac3d561150fc3732`
+const lastReleased = (currentYear, currentMonth) => (
+  `https://api.rawg.io/api/games?page_size=24&dates=${currentYear}-${currentMonth}-01,${currentYear}-${currentMonth}-30&key=888f6e198d894fcdac3d561150fc3732`
 );
 
-const upcoming = (currentYear) => (
-  `https://api.rawg.io/api/games?
-    page_size=50dates=${currentYear}-01-01,${currentYear}-12-31&ordering=-added
-    &key=888f6e198d894fcdac3d561150fc3732`
+const upcoming = (currentYear, currentMonth, nextMonth, today) => (
+  `https://api.rawg.io/api/games?page_size=24&dates=${currentYear}-${currentMonth}-${today},${currentYear}-${nextMonth}-${today}&ordering=-added&key=888f6e198d894fcdac3d561150fc3732`
 );
 
 function urlHandler(url) {
-  const currentYear = new Date().getFullYear();
-  console.log(currentYear);
-  // takes a parameter based on some state
+  const currentYear = new Date().getFullYear().toString();
+  const currentMonth = (new Date().getMonth() + 1).toString().padStart(2, '0');
+  const nextMonth = (new Date().getMonth() + 2).toString().padStart(2, '0');
+  const today = (new Date().getDate()).toString().padStart(2, '0');
+
   const RETURN_URL = {
     popular: popular(currentYear),
     highestrated: highestRated(currentYear),
-    recentlyreleased: lastReleased(currentYear),
-    upcoming: upcoming(currentYear),
+    recentlyreleased: lastReleased(currentYear, currentMonth),
+    upcoming: upcoming(currentYear, currentMonth, nextMonth, today),
   };
   return RETURN_URL[url];
 }
